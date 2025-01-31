e50c8f16eb9b80868ca79ccb58757b65361303b9
BrenoDiamantino <brenolemesd@gmail.com>
2025-01-31 14:21:12 -0300

    add Home page with background image and toggleable info card; update dependencies for Material-UI

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import backgroundhome from "../assets/background-home.svg";
+import "./home.css";
+import Button from "@mui/material/Button"; // Corrigido o import do botão do Material-UI
 
 const Home = () => {
-  return <div>Home</div>;
+  const [cardVisible, setCardVisible] = useState(false);
+
+  const handleButtonClick = () => {
+    setCardVisible(!cardVisible);
+  };
+
+  return (
+    <div className="home-background" style={{ height: "100vh" }}>
+      <div className={`info-card ${cardVisible ? "visible" : ""}`}>
+        <h2>Francauto Locadora</h2>
+        <p>
+          Oferecemos aluguel de veículos com frota moderna com segurança e
+          praticidade!
+        </p>
+      </div>
+      <Button
+        variant="contained"
+        className="about-button"
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          right: "20px",
+          background: "	#00008B",
+        }} // Adiciona o estilo do botão
+        onClick={handleButtonClick} // Adiciona o evento de clique
+      >
+        Sobre Nós!
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
