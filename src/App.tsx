3b382ab77971be380b830bb9ca80f03f4c679d41
BrenoDiamantino <brenolemesd@gmail.com>
2025-01-31 09:14:49 -0300

    update App component to include navigation and pages, change background color in CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import LogoFrancautoLocadora from "./assets/logo-francauto-locadora.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Contatenos from "./pages/contate-nos";
+import Duvidas from "./pages/duvidas";
+import Home from "./pages/home";
+import Localizacao from "./pages/localizacao";
+import Planos from "./pages/planos";
+import NavigationMenu from "./components/navigation-menu";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>import das pages</>;
+  return (
+    <>
+      <NavigationMenu />
+      <Home />
+      <Localizacao />
+      <Planos />
+      <Contatenos />
+      <Duvidas />
+    </>
+  );
 }
 
 export default App;
