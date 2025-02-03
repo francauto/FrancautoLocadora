9154ce332d59ae393d283c5bb47de25a54f7c26b
BrenoDiamantino <brenolemesd@gmail.com>
2025-02-03 10:16:21 -0300

    add Google Maps integration and update Localizacao page layout; enhance styles and add button

---
+++ src/components/google-maps/google-map-component.jsx
@@ -0,0 +1,23 @@
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "100%",
+  height: "400px",
+};
+
+const center = {
+  lat: -23.55052, // Latitude de SP (exemplo)
+  lng: -46.633308, // Longitude de SP (exemplo)
+};
+
+const GoogleMapComponent = () => {
+  return (
+    <LoadScript googleMapsApiKey="AIzaSyBdYOXngqacNlYb6JJlTWBibmc9xcwTJpY">
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+        <Marker position={center} />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default GoogleMapComponent;
--- src/pages/localizacao/localizacao.tsx
+++ src/pages/localizacao/localizacao.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "@dotlottie/player-component"; // Import the Lottie player component
 import "./localizacao.css"; // Import the CSS file
+import LocalizacaoImage from "../../assets/localizacao-francauto.svg"; // Import the SVG image
+import Button from "@mui/material/Button"; // Import the Material UI Button
+import GoogleMapComponent from "../../components/google-maps/google-map-component"; // Import the GoogleMapComponent
 
 const Localizacao = () => {
   return (
@@ -10,13 +13,35 @@ const Localizacao = () => {
           src="https://lottie.host/6aebbfe8-164f-483a-9f6f-9cf20b59e857/0SEBNudFPu.lottie"
           background="transparent"
           speed="1"
-          style={{ width: "300px", height: "300px" }}
+          style={{ width: "400px", height: "400px" }}
           loop
           autoplay
         ></dotlottie-player>
+        <div className="localizacao-label">LOCALIZAÇÃO</div>
+        <div className="localizacao-address-container">
+          <dotlottie-player
+            src="https://lottie.host/ae0b12fc-639c-4c8c-b9b2-c4ed637b9662/Z1MlOrSyY2.lottie"
+            background="transparent"
+            speed="1"
+            style={{ width: "50px", height: "50px" }}
+            loop
+            autoplay
+          ></dotlottie-player>
+          <div className="localizacao-address">
+            Avenida Doutor Severino Tostes Meirelles,<br></br>1660 - São Miguel,
+            Franca São Paulo
+          </div>
+        </div>
       </div>
       <div className="google-maps-container">
-        {/* Google Maps component will be added here */}
+        <GoogleMapComponent /> {/* Add the GoogleMapComponent */}
+        <Button
+          variant="contained"
+          color="primary"
+          className="localizacao-button"
+        >
+          Vamos lá!
+        </Button>
       </div>
     </div>
   );
