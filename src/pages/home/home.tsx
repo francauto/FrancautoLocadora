7628b01a1e718041605219d407e95e0ab929f117
BrenoDiamantino <brenolemesd@gmail.com>
2025-02-03 08:55:55 -0300

    fix import path for CardHome component in Home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import backgroundhome from "../../assets/background-home.svg";
 import "./home.css";
-import CardHome from "../card-home"; // Import the new CardHome component
+import CardHome from "../../components/card-home/card-home"; // Import the new CardHome component
 
 const Home = () => {
   return (
