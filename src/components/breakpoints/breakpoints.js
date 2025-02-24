99dedd2be57c5e3008f82b16b0e0412dfd626c6f
BrenoLemesD <brenolemesd@gmail.com>
2025-02-24 11:04:51 -0300

    feat(navigation-menu): add styled components for navigation menu and breakpoints

---
+++ src/components/breakpoints/breakpoints.js
@@ -0,0 +1,13 @@
+const size = {
+  sm: "300px",
+  md: "950px",
+  bg: "1240px",
+};
+
+const breakpoints = {
+  sm: `(max-width: ${size.sm})`,
+  md: `(max-width: ${size.md})`,
+  bg: `(max-width: ${size.bg})`,
+};
+
+export default breakpoints;
