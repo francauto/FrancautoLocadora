3d535f848c9f81fd3a86b6aa6dfb13d82eea49f9
BrenoLemesd <brenolemesd@gmail.com>
2025-02-24 14:24:56 -0300

    refactor(home): remove CSS file and implement styled-components for video background

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,24 +1,93 @@
-import React, { useState } from "react";
-import "@dotlottie/player-component"; // Import the Lottie player
-import "./home.css";
- // Import the new CardHome component
-import videoHome from "../../assets/video_home.mp4"; // Import the video file
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components";
+import breakpoints from "../../components/breakpoints/breakpoints";
+import videoHome from "../../assets/video_home.mp4";
 
 const Home = () => {
-  const [isVisible, setIsVisible] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-  const toggleCardVisibility = () => {
-    setIsVisible(!isVisible);
-  };
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.muted = true;
+      videoRef.current.play().catch(error => {
+        console.error("Autoplay failed:", error);
+      });
+    }
+  }, []);
 
   return (
-    <div className="home-background">
-      <video className="home-video" src={videoHome} autoPlay loop muted />
-     
-        {/* Se for manter o card, faça o import aqui */}
-        
-      </div>
+    <StyledHomeBackground>
+      <StyledVideoHome ref={videoRef} src={videoHome} autoPlay loop muted playsInline />
+    </StyledHomeBackground>
   );
 };
 
 export default Home;
+
+// Styled Components
+const StyledHomeBackground = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 98.8vw;
+  margin: 0;
+  padding: 0;
+  overflow: hidden; /* Adicionado para evitar rolagem horizontal */
+
+  @media ${breakpoints.xl} {
+    /* Estilos para telas muito grandes */
+    height: 100vh;
+  }
+
+  @media ${breakpoints.lg} {
+    /* Estilos para telas grandes (desktops) */
+    height: 100vh;
+  }
+
+  @media ${breakpoints.md} {
+    /* Estilos para tablets */
+    height: 70vh;
+  }
+
+  @media ${breakpoints.sm} {
+    /* Estilos para celulares grandes */
+    height: 60vh;
+  }
+
+  @media ${breakpoints.xs} {
+    /* Estilos para celulares pequenos */
+    height: 50vh;
+  }
+`;
+
+const StyledVideoHome = styled.video`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
+
+  @media ${breakpoints.xl} {
+    /* Estilos para telas muito grandes */
+  }
+
+  @media ${breakpoints.lg} {
+    /* Estilos para telas grandes (desktops) */
+  }
+
+  @media ${breakpoints.md} {
+    /* Estilos para tablets */
+  }
+
+  @media ${breakpoints.sm} {
+    /* Estilos para celulares grandes */
+  }
+
+  @media ${breakpoints.xs} {
+    /* Estilos para celulares pequenos */
+  }
+`;
