39009f7591507f25fef555320e11ef4e3d626627
BrenoLemesd <brenolemesd@gmail.com>
2025-02-26 12:42:25 -0300

    refactor(localizacao): simplify component structure and enhance responsive design for better layout

--- src/pages/localizacao/localizacao.tsx
+++ src/pages/localizacao/localizacao.tsx
@@ -1,7 +1,6 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import "@dotlottie/player-component";
 import styled from "styled-components";
-import breakpoints from "../../components/breakpoints/breakpoints";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
@@ -18,11 +17,11 @@ L.Icon.Default.mergeOptions({
 });
 
 const Localizacao = () => {
-  const lottiePlayerRef = useRef<HTMLElement>(null);
+  const lottiePlayerRef = useRef(null);
 
   return (
     <StyledLocalizacaoContainer>
-      <StyledLottiePlayerContainer lottiePlayerRef={lottiePlayerRef}>
+      <StyledLottiePlayerContainer>
         <dotlottie-player
           ref={lottiePlayerRef}
           src="https://lottie.host/6aebbfe8-164f-483a-9f6f-9cf20b59e857/0SEBNudFPu.lottie"
@@ -44,13 +43,17 @@ const Localizacao = () => {
               autoplay
             />
             <StyledLocalizacaoAddress>
-              Avenida Doutor Severino Tostes Meirelles,<br />1660 - São Miguel,
-              Franca São Paulo
+              Avenida Doutor Severino Tostes Meirelles,
+              <br />
+              1660 - São Miguel, Franca São Paulo
             </StyledLocalizacaoAddress>
           </StyledAddressWithIcon>
         </StyledLocalizacaoAddressContainer>
       </StyledLottiePlayerContainer>
-      <StyledMapContainer center={[-20.545735817532602, -47.42496940963758]} zoom={15}>
+      <StyledMapContainer
+        center={[-20.545735817532602, -47.42496940963758]}
+        zoom={15}
+      >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <Marker position={[-20.545735817532602, -47.42496940963758]}>
           <Popup>Francauto - Concessionária</Popup>
@@ -62,58 +65,92 @@ const Localizacao = () => {
 
 export default Localizacao;
 
-// Styled Components
 const StyledLocalizacaoContainer = styled.div`
   display: flex;
   align-items: center;
   padding: 20px;
   box-sizing: border-box;
   width: 100%;
-  margin-left: -150px;
+  margin-left: 0px;
 
-  padding-left: 0px; /* Removendo a margem inicial */
+    flex-direction: row;
+    justify-content: center;
 
-  /* Aplica a margem da esquerda conforme a tela aumenta */
-  @media (min-width: 550px) {
-    padding-left: 40px;
-  }
+    @media (max-width: 1920px) {
 
-  @media (min-width: 600px) {
-    padding-left: 60px;
-  }
+    }
 
-  @media (min-width: 650px) {
-    padding-left: 80px;
-  }
+    @media (max-width: 1400px) {
+    
+    }
 
-  @media (min-width: 700px) {
-    padding-left: auto;
-  }
+    @media (max-width: 1200px) {
+        margin-left: -15vh;
+    }
 
-  @media (min-width: 750px) {
-    padding-left: 120px; /* Margem maior */
-  }
+    @media (max-width: 1024px) {
+        flex-direction: column;
+        align-items: center;
+        margin-left: 0;
+    }
 
-  @media ${breakpoints.lg} {
-    flex-direction: column; /* Stack items in a column */
-    align-items: center;
-     margin-left: -250px;
-  }
+    @media (max-width: 900px) {
+        margin-left: -10vh;
+    }
+
+   @media (max-width: 800px) {
+        margin-left: -15vh;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    @media (max-width: 768px) {
+        margin-left: -13vh;
+    }
+    @media (max-width: 700px) {
+        margin-left: -20vh;
+    }
+
+    @media (max-width: 650px) {
+        margin-left: -25vh;
+    }
+
+    @media (max-width: 630px) {
+        margin-left: -28vh;
+    }
+
+    @media (max-width: 550px) {
+        margin-left: -32vh;
+    }
+
+    @media (max-width: 500px) {
+        margin-left: -36vh;
+    }
+    @media (max-width: 480px) {
+      margin-left: -35vh;
+    }
+
+    @media (max-width: 450px) {
+        margin-left: -38vh;
+    }
+
+    @media (max-width: 430px) {
+        margin-left: -38vh;
+    }
+
 `;
 
-const StyledLottiePlayerContainer = styled.div<{ lottiePlayerRef?: React.RefObject<HTMLElement> }>`
+const StyledLottiePlayerContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  width: 90%;
-
+  width: 50%;
   margin-bottom: 20px;
 
-  @media ${breakpoints.lg} {
-    width: 50%;
-   
-  }
+    @media (max-width: 900px) {
+        width: 100%;
+    }
 `;
 
 const StyledLocalizacaoLabel = styled.div`
@@ -144,12 +181,34 @@ const StyledLocalizacaoAddress = styled.div`
   font-size: 16px;
   color: black;
   font-family: "Open Sans", sans-serif;
-  text-align: center; 
-  margin-left: 10px; // Add some space between the icon and the text
+  text-align: center;
+  margin-left: 10px;
 `;
 
 const StyledMapContainer = styled(MapContainer)`
-  width: 400px;
-  height: 400px; // Reduced height
+  width: 45vh;
+  height: 45vh;
   margin-top: 20px;
+
+  
+    @media (max-width: 1000px) {
+        width: 50%;
+    }
+        
+    @media (max-width: 900px) {
+        width: 50%;
+    }
+
+    @media (max-width: 800px) {
+        width: 50%;  /* Smaller map on 800px breakpoint */
+ 
+    }
+
+    @media (max-width: 600px) {
+        height: 300px;
+    }
+    
+    @media (max-width: 500px) {
+    width: 40%;
+    }
 `;
