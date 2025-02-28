a148f977c321c51f987dae8a492fbda4050e456b
BrenoLemesd <brenolemesd@gmail.com>
2025-02-28 12:06:01 -0300

    refactor(home): implement CSS styles for home page layout and video background

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,7 @@
+// Home.tsx
 import React, { useRef, useEffect } from "react";
-import styled from "styled-components";
-import breakpoints from "../../components/breakpoints/breakpoints";
 import videoHome from "../../assets/video_home.mp4";
+import "./home.css"; // Importa o arquivo CSS
 
 const Home = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -16,78 +16,18 @@ const Home = () => {
   }, []);
 
   return (
-    <StyledHomeBackground>
-      <StyledVideoHome ref={videoRef} src={videoHome} autoPlay loop muted playsInline />
-    </StyledHomeBackground>
+    <div className="home-background"> {/* Usa a classe CSS */}
+      <video
+        ref={videoRef}
+        className="video-home" /* Usa a classe CSS */
+        src={videoHome}
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+    </div>
   );
 };
 
 export default Home;
-
-// Styled Components
-const StyledHomeBackground = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 98.8vw;
-  margin: 0;
-  padding: 0;
-  overflow: hidden; /* Adicionado para evitar rolagem horizontal */
-
-  @media ${breakpoints.xl} {
-    /* Estilos para telas muito grandes */
-    height: 100vh;
-  }
-
-  @media ${breakpoints.lg} {
-    /* Estilos para telas grandes (desktops) */
-    height: 100vh;
-  }
-
-  @media ${breakpoints.md} {
-    /* Estilos para tablets */
-    height: 70vh;
-  }
-
-  @media ${breakpoints.sm} {
-    /* Estilos para celulares grandes */
-    height: 60vh;
-  }
-
-  @media ${breakpoints.xs} {
-    /* Estilos para celulares pequenos */
-    height: 50vh;
-  }
-`;
-
-const StyledVideoHome = styled.video`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  z-index: -1;
-
-  @media ${breakpoints.xl} {
-    /* Estilos para telas muito grandes */
-  }
-
-  @media ${breakpoints.lg} {
-    /* Estilos para telas grandes (desktops) */
-  }
-
-  @media ${breakpoints.md} {
-    /* Estilos para tablets */
-  }
-
-  @media ${breakpoints.sm} {
-    /* Estilos para celulares grandes */
-  }
-
-  @media ${breakpoints.xs} {
-    /* Estilos para celulares pequenos */
-  }
-`;
